5dc8b23180081a9cbb4d5cf9bd091d13c2ce5bc6
Dennis Nkangi <dennis.nkangi@gmail.com>
2014-08-20 13:51:07 +0200

    Added nuspec file. Changed to auto increment assembly version.

--- Avida.FinancialUtility/Properties/AssemblyInfo.cs
+++ Avida.FinancialUtility/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Avida.FinancialUtility")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library of utility methods for financial calculations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Avida Finans AB")]
 [assembly: AssemblyProduct("Avida.FinancialUtility")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Avida Finans AB 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
